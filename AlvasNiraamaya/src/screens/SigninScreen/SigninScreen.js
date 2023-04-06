45374dea1a660223c252859f5ac4650631f73a6b
Anesh Soamanath Majalikar <64136492+AneshSM@users.noreply.github.com>
2023-04-06 22:26:38 +0530

    update

--- AlvasNiraamaya/src/screens/SigninScreen/SigninScreen.js
+++ AlvasNiraamaya/src/screens/SigninScreen/SigninScreen.js
@@ -35,36 +35,6 @@ const SigninScreen = () => {
     navigation.navigate('DeskTop');
   };
 
-  const styles = StyleSheet.create({
-    root: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      flexGrow: 1,
-      height: '100%',
-      width: '100%',
-    },
-    root_view: {
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      flexDirection: 'column',
-      flexGrow: 1,
-      borderWidth: 1,
-    },
-    container: {
-      width: '100%',
-      backgroundColor: COLORS.clr60,
-      padding: 20,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
-      flexDirection: 'column',
-      flex: 2,
-    },
-  });
-
   const onForgotPasswordPressed = () => {
     console.warn('Forgot Password');
     navigation.navigate('ForgotPassword');
@@ -75,59 +45,57 @@ const SigninScreen = () => {
   };
 
   return (
-    <View style={styles.root}>
-      <ScrollView showsVerticalScrollIndicator={false} style={styles.root}>
-        <View style={[styles.root_view]}>
-          <View style={[SigninScreen_Style.head]}>
-            <View style={[SigninScreen_Style.logo]}>
-              <Image
-                source={IMGS.logo}
-                style={[styles.logo, SigninScreen_Style.image]}
-                resizeMode="contain"></Image>
-            </View>
-            <View style={[SigninScreen_Style.title]}>
-              <Text style={[SigninScreen_Style.text]}>{ROUTES.TITLE}</Text>
-            </View>
-          </View>
-          <View style={[styles.container]}>
-            <CustomeInput
-              placeholder="Username"
-              name="Username"
-              control={control}
-              rules={{required: 'Username is required'}}
-            />
-            <CustomeInput
-              placeholder="Password"
-              name="Password"
-              control={control}
-              rules={{
-                required: 'Password is required',
-                minLength: {
-                  value: 8,
-                  message: 'Password should be minimum 8 characters long',
-                },
-              }}
-              secureTextEntry
-            />
-
-            <CustomeButton
-              text="LogIn"
-              onPress={handleSubmit(onSiginPressed)}
-            />
-            <CustomeButton
-              text="Forgot Password ?"
-              onPress={onForgotPasswordPressed}
-              type="Tertiary"
-            />
-            <SocialSignInButtons />
-            <CustomeButton
-              text="Don't have an account ? Create one"
-              onPress={onSignUpPressed}
-              type="Tertiary"
-            />
-          </View>
+    <View style={SigninScreen_Style.root}>
+      <View style={[SigninScreen_Style.head]}>
+        <View style={[SigninScreen_Style.logo]}>
+          <Image
+            source={IMGS.logo}
+            style={[SigninScreen_Style.image]}
+            resizeMode="contain"
+          />
         </View>
-      </ScrollView>
+        <View style={[SigninScreen_Style.title]}>
+          <Text style={[SigninScreen_Style.text]}>{ROUTES.TITLE}</Text>
+        </View>
+      </View>
+      <View style={[SigninScreen_Style.container]}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={SigninScreen_Style.form}>
+          <CustomeInput
+            placeholder="Username"
+            name="Username"
+            control={control}
+            rules={{required: 'Username is required'}}
+          />
+          <CustomeInput
+            placeholder="Password"
+            name="Password"
+            control={control}
+            rules={{
+              required: 'Password is required',
+              minLength: {
+                value: 8,
+                message: 'Password should be minimum 8 characters long',
+              },
+            }}
+            secureTextEntry
+          />
+
+          <CustomeButton text="LogIn" onPress={handleSubmit(onSiginPressed)} />
+          <CustomeButton
+            text="Forgot Password ?"
+            onPress={onForgotPasswordPressed}
+            type="Tertiary"
+          />
+          <SocialSignInButtons />
+          <CustomeButton
+            text="Don't have an account ? Create one"
+            onPress={onSignUpPressed}
+            type="Tertiary"
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 };
